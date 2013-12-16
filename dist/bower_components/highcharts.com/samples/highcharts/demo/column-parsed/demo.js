5bb419736ab1c5cce4b7ddbce583da7e03ecbb33
Victor J. Reventos <victor.reventos1@gmail.com>
2013-12-16 19:23:11 -0400

    Updates

---
+++ dist/bower_components/highcharts.com/samples/highcharts/demo/column-parsed/demo.js
@@ -0,0 +1,25 @@
+$(function () {
+    $('#container').highcharts({
+        data: {
+            table: document.getElementById('datatable')
+        },
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Data extracted from a HTML table in the page'
+        },
+        yAxis: {
+            allowDecimals: false,
+            title: {
+                text: 'Units'
+            }
+        },
+        tooltip: {
+            formatter: function() {
+                return '<b>'+ this.series.name +'</b><br/>'+
+                    this.y +' '+ this.x.toLowerCase();
+            }
+        }
+    });
+});
---
+++ dist/bower_components/highcharts.com/samples/stock/plotoptions/candlestick-linecolor/demo.js
@@ -0,0 +1,25 @@
+$(function() {
+	$('#container').highcharts('StockChart', {
+	    
+	    chart: {
+	    },
+	    
+	    plotOptions: {
+	    	candlestick: {
+	    		lineColor: '#2f7ed8',	    		
+	    		upLineColor: 'silver', // docs
+	    		upColor: 'silver'
+	    	}
+	    },
+	    
+	    rangeSelector: {
+	    	selected: 1
+	    },
+	    
+	    series: [{
+	    	type: 'candlestick',
+	        name: 'USD to EUR',
+	        data: ohlcdata
+	    }]
+	});
+});
